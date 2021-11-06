1f98443ad961da192d029ead2e394b8fae41384d
sebasrengi <sebas.rengi@hotmail.com>
2021-11-06 14:46:55 -0500

    _init_base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,3 +3,13 @@
 import uuid
 import models
 from datetime import datetime
+class BaseModel:
+    """class Base Model"""
+    def __init__(self, *args, **kwargs):
+        """ initialize Base Model """
+	if not kwargs:
+		self.id = str(uuid.uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = self.created_at
+		models.storage.new(self)
+	else:
