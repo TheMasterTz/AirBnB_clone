fcecd307a2348202ec25ad8a1c48e6932b21970e
sebasrengi <sebas.rengi@hotmail.com>
2021-11-06 14:27:57 -0500

    fixed1

---
+++ models/engine/file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""file storage class"""
+import json
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
