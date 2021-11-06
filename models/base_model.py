d1f4f094dc46cd618659032e9af33df048542135
sebasrengi <sebas.rengi@hotmail.com>
2021-11-06 15:11:17 -0500

    base_model1

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,27 @@ import models
 from datetime import datetime
 class BaseModel:
     """class Base Model"""
-    def __init__(self, *args, **kwargs):
+	def __init__(self, *args, **kwargs):
         """ initialize Base Model """
-	if not kwargs:
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = self.created_at
-		models.storage.new(self)
-	else:
+		if not kwargs:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = self.created_at
+			models.storage.new(self)
+		else:
+            		for arg, val in kwargs.items():
+                		if arg in ('created_at', 'updated_at'):
+                    		val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+
+                		if arg != '__class__':
+                    			setattr(self, arg, val)
+
+	def __str__(self):
+        """string representation"""
+        	return "[{0}] ({1}) {2}".format(
+            	self.__class__.__name__, self.id, self.__dict__)
+
+	def save(self):
+        """save method"""
+        	self.updated_at = datetime.now()
+        	models.storage.save()
