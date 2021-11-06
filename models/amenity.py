7193fabbad43265d443b2bcfe7f50687edf5742f
sebasrengi <sebas.rengi@hotmail.com>
2021-11-06 15:53:26 -0500

    merge2

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-=======
 #!/usr/bin/python3
->>>>>>> testing
 """User Class"""
 from models.base_model import BaseModel
-
-
 class Amenity(BaseModel):
-<<<<<<< HEAD
+
     name = ""
-=======
-        name = ""
->>>>>>> testing
--- models/city.py
+++ models/city.py
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-=======
 #!/usr/bin/python3
->>>>>>> testing
 """User Class"""
 from models.base_model import BaseModel
-
-
 class City(BaseModel):
-<<<<<<< HEAD
     """City Class inherit from BaseModel"""
-
     state_id = ""
     name = ""
-=======
-        """City Class inherit from BaseModel"""
-
-            state_id = ""
-            name = ""
->>>>>>> testing
